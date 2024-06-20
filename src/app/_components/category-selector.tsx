7017cba361840b36d2ae850b6c4e9c5a67024789
Adriano Rocha <adriano.olr@gmail.com>
2024-06-19 21:08:29 -0300

    feat(random jokes): add a simple joke category selector

---
+++ src/app/_components/category-selector.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+
+type Props = {
+  categories: string[];
+};
+
+export default function CategorySelector({ categories }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const selected = searchParams.get("category") ?? undefined;
+
+  const handleChange = useCallback(
+    (category: string) => {
+      const newSearchParams = new URLSearchParams(searchParams);
+      newSearchParams.set("category", category);
+      router.push(`${pathname}?${newSearchParams.toString()}`);
+    },
+    [pathname, router, searchParams]
+  );
+
+  return (
+    <Select value={selected} onValueChange={handleChange}>
+      <SelectTrigger className="capitalize">
+        <SelectValue placeholder="Select a category" />
+      </SelectTrigger>
+
+      <SelectContent>
+        {categories.map((category) => (
+          <SelectItem key={category} value={category} className="capitalize">
+            {category}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,33 @@
+import CategorySelector from "./_components/category-selector";
 import RevealButton from "./_components/reveal-button";
 
-export default async function Home() {
-  const joke = await fetch("https://api.chucknorris.io/jokes/random", {
+type PageProps = {
+  searchParams: {
+    category: string;
+  };
+};
+
+export default async function Home({ searchParams }: PageProps) {
+  const url = new URL("https://api.chucknorris.io/jokes/random");
+  url.searchParams.set("category", searchParams.category);
+
+  console.log(url.toString());
+  const joke = await fetch(url, {
     cache: "no-cache",
   }).then((res) => res.json());
+
+  const categories: string[] = await fetch(
+    "https://api.chucknorris.io/jokes/categories",
+    {
+      cache: "no-cache",
+    }
+  ).then((res) => res.json());
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-12 p-24">
       <h1>Hello Chuck!</h1>
+
+      <CategorySelector categories={categories} />
 
       <RevealButton>{joke.value}</RevealButton>
     </div>
