f8ccd95a197ef34f8db2529d0605e240d557d56c
Adriano Rocha <adriano.olr@gmail.com>
2024-06-23 12:41:57 -0300

    fix: hide joke when the selected category changes

--- src/app/_components/reveal-button.tsx
+++ src/app/_components/reveal-button.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import Button from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { showDifferentJoke } from "../actions";
+import { useSearchParams } from "next/navigation";
 
 type Props = {
   children: React.ReactNode;
 };
 export default function RevealButton({ children }: Props) {
   const [show, setShow] = useState(false);
+
+  const searchParams = useSearchParams();
+  const category = searchParams.get("category");
+
+  useEffect(() => {
+    setShow(false);
+  }, [category]);
 
   return (
     <>
