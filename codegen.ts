8a9a0296e8a68938936c74965440225ec6230a0b
Adriano Rocha <adriano.olr@gmail.com>
2024-06-23 17:28:41 -0300

    fix: add dotenv import for codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,5 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
+import "dotenv/config";
+
 const config: CodegenConfig = {
   schema: process.env.GRAPHQL_URL,
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
